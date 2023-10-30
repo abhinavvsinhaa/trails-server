659e3a72d35fb35b1193ff238f45094ef28aa011
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-10-30 17:46:41 +0530

    Initialized with Express & Typescript

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app: Express = express()
+const port: string = process.env.PORT || '8080' 
+
+app.use('/', (req: Request, res: Response) => {
+    res.send('Welcome to Trails!')
+})
+
+app.listen(port, () => {
+    console.log(`trails-server up and running at port: ${port}`);
+})
