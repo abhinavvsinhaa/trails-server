02080a16bea97c1df0c04f3428885dc8ebf9742a
Abhinav Sinha <abhinavvsinhaa@gmail.com>
2023-10-31 20:01:24 +0530

    updated blog and topic model, added husky and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express, { Express, Request, Response } from 'express'
-import dotenv from 'dotenv'
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
-const port: string = process.env.PORT || '8080' 
+const app: Express = express();
+const port: string = process.env.PORT || "8080";
 
-app.use('/', (req: Request, res: Response) => {
-    res.send('Welcome to Trails!')
-})
+app.use("/", (req: Request, res: Response) => {
+	res.send("Welcome to Trails!");
+});
 
 app.listen(port, () => {
-    console.log(`trails-server up and running at port: ${port}`);
-})
+	console.log(`trails-server up and running at port: ${port}`);
+});
